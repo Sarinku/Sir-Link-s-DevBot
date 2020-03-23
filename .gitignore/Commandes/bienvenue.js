cca419207ac1b16b1f2d17c1863c9b87c4bd34b0
Sir Link <sirlink.developer@gmail.com>
2020-03-23 14:26:45 +0100

    Update bienvenue.js

--- .gitignore/Commandes/bienvenue.js
+++ .gitignore/Commandes/bienvenue.js
@@ -2,6 +2,6 @@ const Discord = require('discord.js');
 
 module.exports.run = (client, message, args) => {
         bot.on("guildMemberAdd", member => {
-            member.guild.channels.find("name", "👋┃𝖡ienvenue").send(`👋 Bienvenue ${member.user.username} ! Nous sommes maintenant ${message.guild.memberCount}`)
+            member.guild.channels.find(`name`, "👋┃𝖡ienvenue").send(`👋 Bienvenue ${member.user.username} ! Nous sommes maintenant ${message.guild.memberCount}`)
         });
 };
