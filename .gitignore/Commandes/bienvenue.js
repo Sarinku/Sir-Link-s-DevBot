96e3de4b8a47370e35b8f3bfacc270fe53736e01
Sir Link <sirlink.developer@gmail.com>
2020-03-23 14:21:05 +0100

    Add files via upload

---
+++ .gitignore/Commandes/bienvenue.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+
+module.exports.run = (client, message, args) => {
+        bot.on("guildMemberAdd", member => {
+            member.guild.channels.find("name", "👋┃𝖡ienvenue").send(`👋 Bienvenue ${member.user.username} ! Nous sommes maintenant ${message.guild.memberCount}`)
+        });
+};
